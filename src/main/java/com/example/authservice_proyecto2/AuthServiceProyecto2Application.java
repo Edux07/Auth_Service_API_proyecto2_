e530062704b60c4aec49fc11ee3da029e4783122
Eduardo Hernández Hernández <hernandezedu22@gmail.com>
2024-09-18 23:50:15 +0200

    Initial commit

---
+++ src/main/java/com/example/authservice_proyecto2/AuthServiceProyecto2Application.java
@@ -0,0 +1,13 @@
+package com.example.authservice_proyecto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceProyecto2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceProyecto2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authservice_proyecto2/AuthServiceProyecto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authservice_proyecto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceProyecto2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
